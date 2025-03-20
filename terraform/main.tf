8e19f95a7169c92e7ce890385ac1777dba5d739e
John <j.naeder324@gmail.com>
2025-03-19 22:49:43 -0400

    Got rid of the startup script. Shit didn't work

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,32 +52,6 @@ resource "google_compute_instance" "web-test" {
     }
   }
 
-  # metadata_startup_script = <<-EOT
-  #   #! /bin/bash
-
-    # Add Docker's official GPG key:
-    # sudo apt-get update
-    # sudo apt-get install ca-certificates curl
-    # sudo install -m 0755 -d /etc/apt/keyrings
-    # sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    # sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # # Add the repository to Apt sources:
-    # echo \
-    #   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    #   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-    #   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    # sudo apt-get update
-
-    # sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin git -y
-
-    # git clone https://github.com/JNaeder/k8-test-1.git
-    # cd /home/j_naeder324/k8-test-1
-
-    # sudo docker compose up --detach
-
-  # EOT
-
   metadata = {
     "ssh-keys" = "j_naeder324:${file("~/.ssh/gcp-ssh-key.pub")}"
   }
