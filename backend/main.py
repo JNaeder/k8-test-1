945331638f7db72f783a462dafae6aa3792b2663
John <j.naeder324@gmail.com>
2025-03-23 12:58:00 -0400

    Updated frontend and backend. Will practice updating VM

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
+import random
 
 app = FastAPI()
 
@@ -12,14 +13,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+quotes = [
+  "Believe you can and you're halfway there.",
+  "Do what you can, with what you have, where you are.",
+  "Dream big and dare to fail.",
+  "Stay hungry, stay foolish.",
+  "Success is not final, failure is not fatal.",
+  "You are stronger than you think.",
+  "The only way to do great work is to love what you do.",
+  "Doubt kills more dreams than failure ever will.",
+  "Everything you’ve ever wanted is on the other side of fear.",
+  "Act as if what you do makes a difference. It does.",
+  "Happiness depends upon ourselves.",
+  "What we achieve inwardly will change outer reality.",
+  "Be yourself; everyone else is already taken.",
+  "It always seems impossible until it’s done.",
+  "With the new day comes new strength and new thoughts."
+]
+
 
 @app.get("/")
 async def root():
     todayDate = datetime.now()
-    print(todayDate)
+    random_num = random.randint(0, len(quotes) - 1)
     return {
         "status": "online",
-        "time": todayDate
+        "time": todayDate,
+        "quote": quotes[random_num]
     }
 
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,45 @@
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import k8Logo from "./assets/k8_logo.svg";
 
 function App() {
   const [backendStatus, setBackendStatus] = useState<String>("");
-  const [backendTime, setBackendTime] = useState<String>("");
+  const [backendDate, setBackendDate] = useState<String>("-");
+  const [backendTime, setBackendTime] = useState<String>("-");
+  const [backendQuote, setBackendQuote] = useState<String>("-");
 
   const handleClick = async () => {
     const backendURL =
       import.meta.env.VITE_BACKEND_URL || "http://127.0.0.1:8000";
     const response = await axios.get(backendURL);
     const data = response.data;
+    const datetime = new Date(data["time"]);
+    setBackendDate(datetime.toDateString());
+    setBackendTime(
+      `${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`
+    );
     setBackendStatus(data["status"]);
-    setBackendTime(data["time"]);
+    setBackendQuote(data["quote"]);
   };
 
   return (
     <>
-      <h1>k8 Test</h1>
+      <div className="container">
+        <img src={k8Logo} height={100} />
+        <h1>k8 Test</h1>
+      </div>
+      <p>⚠️currently not running on k8⚠️</p>
+      <h2>{backendQuote}</h2>
+      <h3>
+        Status: {backendStatus.toUpperCase()}{" "}
+        {backendStatus == "online" ? "✅" : "❌"}
+      </h3>
+      <h3>Date: {backendDate}</h3>
+      <h3>Time: {backendTime}</h3>
       <button onClick={handleClick}>Test Backend</button>
-      <p>Status: {backendStatus}</p>
-      <p>Time: {backendTime}</p>
       <div></div>
+      <br />
       <button
         onClick={() => {
           window.open("https://github.com/JNaeder/k8-test-1", "_blank");
