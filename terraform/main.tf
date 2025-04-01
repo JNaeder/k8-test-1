eed63ad496837fbbb405213bffddcaf4bdf1a862
John <j.naeder324@gmail.com>
2025-03-31 22:42:02 -0400

    Updated terraform to include GKE, wrote script to deploy k8

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,22 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.25.0"
     }
   }
 }
 
+variable "project_id" {}
+variable "external_ip" {}
+variable "region" {}
+variable "zone" {}
+variable "domain_name" {}
+
 provider "google" {
-  project = "gcp-practice-1-453919"
-  region  = "us-east1"
-  zone    = "us-east1-b"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "mynetwork" {
@@ -18,48 +24,36 @@ resource "google_compute_network" "mynetwork" {
   auto_create_subnetworks = true
 }
 
-resource "google_compute_firewall" "allow-ssh" {
-  name        = "allow-ssh"
-  network     = google_compute_network.mynetwork.name
-  description = "Allow SSH"
-  source_ranges = ["0.0.0.0/0"]
-
-  allow {
-    protocol  = "tcp"
-    ports     = ["22"]
-  }
-}
 
 resource "google_compute_firewall" "allow-web" {
-  name        = "allow-web"
-  network     = google_compute_network.mynetwork.name
-  description = "Allow Web Traffic"
+  name          = "allow-web"
+  network       = google_compute_network.mynetwork.name
+  description   = "Allow Web Traffic"
   source_ranges = ["0.0.0.0/0"]
 
   allow {
-    protocol  = "tcp"
-    ports     = ["80", "443", "8000"]
+    protocol = "tcp"
+    ports    = ["80", "443"]
   }
 }
 
-resource "google_compute_instance" "web-test" {
-  name         = "web-test"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20250313"
-    }
-  }
-
-  metadata = {
-    "ssh-keys" = "j_naeder324:${file("~/.ssh/gcp-ssh-key.pub")}"
-    "startup-script"  = "sudo apt update\nsudo apt install git -y\n cd /home/j_naeder324\ngit clone https://github.com/JNaeder/k8-test-1.git"
-  }
-
-  network_interface {
-    network = google_compute_network.mynetwork.name
-    access_config {
-    }
-  }
+resource "google_container_cluster" "my-cluster" {
+  name                = "my-cluster"
+  deletion_protection = false
+  enable_autopilot    = true
+  network             = google_compute_network.mynetwork.name
 }
+
+resource "google_dns_managed_zone" "audiobytes-zone" {
+  name        = "audiobytes"
+  dns_name    = "audiobytes.app."
+}
+
+resource "google_dns_record_set" "a-record" {
+  name         = var.domain_name
+  managed_zone = google_dns_managed_zone.audiobytes-zone.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [var.external_ip]
+}
+
